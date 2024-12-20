cc58a44f5322bd479dc36326bbf29c04679d8e49
nico <nicozalazar67@gmail.com>
2024-12-20 15:18:29 -0300

    mejora de botones, alertas y funciones

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.querySelector("form").addEventListener("submit", (e) => {
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    if (!name || !email || !message) {
-        alert("Por favor, completa todos los campos del formulario.");
-        e.preventDefault();
-    }
-});
+//Declaración de variables globales
 const productsContainer = document.querySelector(".productos-container");
 const cartContainer = document.querySelector(".cart-container");
 let products = [];
@@ -15,10 +6,12 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 const btnCart = document.getElementById("btnCart");
 const btnUp = document.getElementById("btnUp");
 
+//Verificar si hay productos en el carrito
 if (cart.length > 0) {
     btnCart.hidden = false;
 }  
 
+//Función para obtener los productos
 fetch("https://fakestoreapi.in/api/products/category?type=mobile")
     .then((response) => response.json())
     .then((resp) => {
@@ -34,12 +27,12 @@ fetch("https://fakestoreapi.in/api/products/category?type=mobile")
                         <h3>${product.title.slice(6)}</h3>
                     </div>
                     <div class="card-description">
-                        <button class="btn-description" data-id=${product.id}>Ver descripción</button>
+                        <button class="btn-description" onclick="showDescription(this)" data-id=${product.id}>Ver descripción</button>
                     </div>
                     <div class="card-price">                    
                     <p>Precio: $${product.price}</p>
                     </div>
-                    <button class="btn-add-cart">Añadir al carrito</button>
+                    <button class="btn-add-cart" onclick="addToCart(this.parentElement)" data-id="${product.id}">Añadir al carrito</button>
                 </div>
             `;
             productsContainer.innerHTML += card;
@@ -47,16 +40,29 @@ fetch("https://fakestoreapi.in/api/products/category?type=mobile")
     })
     .catch((error) => console.error("Error al obtener los productos:", error));
 
-    
-document.body.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn-add-cart")) {
-        const card = e.target.closest(".card");
+   
+/**
+ * Agrega un producto al carrito de compras.
+ * Si el producto ya existe en el carrito, incrementa su cantidad en 1.
+ * De lo contrario, agrega el producto al carrito con una cantidad de 1.
+ * Actualiza el contador de productos en el carrito y muestra un mensaje de éxito.
+ * @param {Element} card - Contenedor del producto a agregar.
+ */
+function addToCart(card) {       
         const product = {
+            id: card.querySelector("button").dataset.id,
             title: card.querySelector("h3").innerText,
-            price: card.querySelector("p").innerText,
+            price: parseFloat(card.querySelector("p").innerText.replace('Precio: $', '')),
+            quantity: 1
         };
 
-        cart.push(product);
+        const existingProductIndex = cart.findIndex((item) => item.id == product.id);
+
+        if (existingProductIndex > -1) {
+            cart[existingProductIndex].quantity += 1;
+        } else {
+            cart.push(product);
+        }
         localStorage.setItem("cart", JSON.stringify(cart));
         btnCart.hidden = false;
         updateCartCount();
@@ -67,58 +73,122 @@ document.body.addEventListener("click", (e) => {
             showConfirmButton: false,
             timer: 1500,
         });
-    }
-});
+};
 
+/**
+ * Actualiza el contador de productos en el carrito en el botón "Ir al carrito" y
+ * en el encabezado de la página.
+ */
 function updateCartCount() {
-    document.getElementById("cart-count").innerText = cart.length;
-    document.getElementById("cart-count-header").innerText = cart.length;
+    let totalQuantity = 0;
+
+    cart.forEach((item) => {
+        totalQuantity += item.quantity;
+    });
+    document.getElementById("cart-count").innerText = totalQuantity;
+    document.getElementById("cart-count-header").innerText = totalQuantity;
 }
 
 document.addEventListener("DOMContentLoaded", updateCartCount);
 
-document.body.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn-description")) {
-        const id = e.target.getAttribute("data-id"); // Captura el ID del botón
-
+/**
+ * Muestra la descripción de un producto en un modal.
+ * @param {Element} btn - Botón que contiene el ID del producto.
+ */
+function showDescription(btn) {
+        const id = btn.dataset.id;
         const encontrado = products.find((product) => product.id == id); 
 
         if (encontrado) {
-            // Actualiza el contenido del modal
             document.getElementById("productModalLabel").innerText = `Descripción de ${encontrado.model}`;
             document.getElementById("productDescription").innerText = encontrado.description;
 
-            // Muestra el modal
             const productModal = new bootstrap.Modal(document.getElementById("productModal"));
             productModal.show();
         } else {
-            alert("Producto no encontrado.");
+           Swal.fire({
+                icon: "error",
+                title: "Error al cargar la descripción",
+                showConfirmButton: false,
+                timer: 1500,
+            });
         }
-    }
-});
+};
 
+/**
+ * Abre el modal del carrito de compras y muestra el contenido
+ * del carrito con los productos agregados.
+ */
 function openCart() {
     const cartModal = new bootstrap.Modal(document.getElementById("cartModal"));
     document.getElementById("cartModalLabel").innerText = "Carrito de compras";
     cartContainer.innerHTML = "";
-    cart.forEach((cart) => {
+
+    let total = 0; 
+    cart.forEach((item, index) => {
         let cartDetail = `
-            <ul>
-                <li>${cart.title}</li>
-                <li>${cart.price}</li>
-            <ul>
+            <div class="cart-item">
+                <p>${item.title}</p>
+                <p>$${item.price}</p>
+                <input type="number" class="item-quantity" data-index="${index}" value="${item.quantity}" min="1">
+                <button class="btn-update-quantity" data-index="${index}">Actualizar</button>
+            </div>
         `;
         cartContainer.innerHTML += cartDetail;
+        total += item.price * item.quantity;
     });
+    const totalPrice = `<p>Total: $${total.toFixed(2)}</p>`;
+    cartContainer.innerHTML += totalPrice;
     cartModal.show();
 }
 
+document.body.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-update-quantity")) {
+        const index = e.target.getAttribute("data-index");
+        const newQuantity = parseInt(document.querySelector(`.item-quantity[data-index="${index}"]`).value);
+
+        if (newQuantity > 0) {
+            cart[index].quantity = newQuantity;
+            localStorage.setItem("cart", JSON.stringify(cart));
+            updateCartCount();
+            openCart();
+        }
+    }
+});
+
+// Cerrar el carrito y actualizar el conteo
+document.getElementById("update-cart").addEventListener("click", () => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartCount();
+    const cartModal = bootstrap.Modal.getInstance(document.getElementById("cartModal"));
+    cartModal.hide();
+});
+
+/**
+ * Limpia el carrito de compras eliminando todos los productos del carrito.
+ * Establece el botón "Ir al carrito" y el enlace "Carrito" en el menú superior en hidden.
+ * Actualiza el conteo de productos en el carrito en el botón "Ir al carrito" y en el encabezado de la página.
+ */
+function clearCart(){
+    localStorage.removeItem("cart");
+    cart = [];
+    btnCart.hidden = true;
+    navcart.hidden = true;
+    updateCartCount();
+    Swal.fire({
+        icon: "success",
+        title: "Carrito vaciado",
+        showConfirmButton: false,
+        timer: 1500,
+    })
+}
+
+// Configurar la visibilidad de los botones
 window.addEventListener("scroll", () => {
     const isAtTop = window.scrollY === 0;
     const isAtDown = window.scrollY > 0;
     const isCartEmpty = cart.length === 0;
 
-    // Configurar la visibilidad de los botones
     btnUp.hidden = isAtTop;
     btnCart.hidden = isAtTop || isCartEmpty;
     navcart.hidden = isAtDown || isCartEmpty;
@@ -129,3 +199,23 @@ btnUp.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
+/**
+ * Valida los campos del formulario de contacto.
+ * Muestra un mensaje de error con sweetalert2 si hay campos vacíos.
+ * @returns {boolean} false si hay campos vacíos, true en caso contrario.
+ */
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (!name || !email || !message) {
+        Swal.fire({
+            icon: "error",
+            title: "Todos los campos son obligatorios",
+            showConfirmButton: true,
+            timer: 1500,
+        })
+        return false;   //TODO EVITAR ENVIO DEL FORMULARIO     
+    }
+}
